4dac1e77c358d9ce275eef7db76fe0bd01b30b96
lucky529 <1248327663@qq.com>
2018-11-27 18:16:54 +0800

    提交代码

---
+++ 11.27/11.27/test.c
@@ -0,0 +1,22 @@
+//int rob(int* nums, int numsSize) 
+//{
+//	int sum1 = 0,sum2 = 0;
+//	for(int i = 0; i < numsSize; i += 2)
+//	{
+//		sum1 += nums[i];
+//	}
+//	for(int j = 0; j < numsSize; j += 2)
+//	{
+//		sum2 +=nums[j];
+//	}
+//	if(sum1 > sum 2)
+//	{
+//		return sum1;
+//	}
+//	else
+//	{
+//		return sum2;
+//	}
+//	return -1;
+//}
+
