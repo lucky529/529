e314e20dbfce87d8ffa01455d5c8cc52d779dcff
lucky529 <1248327663@qq.com>
2019-02-15 23:30:33 +0800

    宏

---
+++ 预定义符号和宏/预定义符号和宏/test.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<windows.h>
+
+int main()
+{
+	/*for (int i = 0; i < 10; i++)
+	{
+		printf("file:%s line:%d date:%s i=%d\n", __FILE__,__LINE__,__DATE__,i);
+	}*/
+	printf("%d", __STDC__);
+	system("pause");
+	return 0;
+}
