3c6e72987fc156e1f3bc33d519dee358230769ba
lucky529 <1248327663@qq.com>
2019-11-23 23:26:07 +0800

    19.11.23

---
+++ 暴打syy/暴打syy/test.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<string>
+#include<windows.h>
+using namespace std;
+
+int main()
+{
+	cout << "请输入暴打石悦悦的方式" << endl;
+	string str;
+	cin >> str;
+	int count = 10;
+	while (count--)
+	{
+		cout << "石悦悦已经死亡，别打了" << endl;
+	}
+	cout << "刘晨翔胜利" << endl;
+	system("pause");
+	return 0;
+}
