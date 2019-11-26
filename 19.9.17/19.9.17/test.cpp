d29e1af944e68691b61977c5cd3e3192711b478c
lucky529 <1248327663@qq.com>
2019-11-26 23:53:47 +0800

    19.11.26

---
+++ 19.9.17/19.9.17/test.cpp
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<iostream>
+using namespace std;
+
+
+//template<class T>函数模板类似于一个模具，在使用时被参数化，根据实参类型产生函数的特定类型版本。
+//模板的推演和实例化
+//如果模板和现成的同时存在，那么就用现成的
+
+
+void func(const int a)
+{
+
+}
+int main()
+{
+	long a = 10;
+	func(10);
+	system("pause");
+	return 0;
+}
